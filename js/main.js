d7a0b37d2c4efd4907158d44913cafb2bb107a42
Will Hinkelman <will@Wills-MacBook-Pro-2.local>
2019-10-25 15:23:47 -0700

    rendered a game message

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ const PLAYER_X = 1;
 const PLAYER_O = -1;
 const EMPTY = 0;
 
+const COLORS = {
+    null: 'red',
+    '1': 'green',
+    '-1': 'purple'
+};
+
+const WINNINGCOMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
 /*----- app's state (variables) -----*/
 let turn, winner, board;
 
@@ -11,10 +28,13 @@ let turn, winner, board;
 /*----- cached element references -----*/
 
 const replayBtn = document.querySelector('button');
+const squares = document.querySelectorAll('div');
+const gameMessage = document.querySelector('h2');
 
 /*----- event listeners -----*/
 
-
+document.querySelector('section').addEventListener('click', playerMove);
+replayBtn.addEventListener('click', init)
 
 /*----- functions -----*/
 
@@ -27,12 +47,41 @@ init();
 //     [0, 0, 0]
 // ]
 
-function inti() {
+// Initialize the board
+function init() {
+    
     board = [
-        [EMPTY, EMPTY, EMPTY],
-        [EMPTY, EMPTY, EMPTY],
-        [EMPTY, EMPTY, EMPTY]
+        null, null, null,
+        null, null, null,
+        null, null, null
     ];
     turn = PLAYER_X;
-    winner = '';
+    winner = null;
+
+    render();
+}
+
+function render() {
+
+    // Sets the color for each square
+    squares.forEach(function(e, idx) {
+        e.style.backgroundColor = `${COLORS[`${board[idx]}`]}`
+    })
+    
+    if (winner === null) {
+        gameMessage.textContent = `It is player ${turn} turn`;
+    } else if (winner === 'T') {
+        gameMessage.textContent = 'It is a tie!';
+    } else {
+        gameMessage.textContent = "Someone Won"
+    }
+}
+
+function playerMove(evt) {
+    let sqrClicked = evt.target;
+    if (sqrClicked.tagName !== 'DIV') {
+        return;
+    }
+    
+    console.log(sqrClicked);
 }
