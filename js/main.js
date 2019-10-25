4c938d72570e4953cde38c0909dcf92bc51c65ec
Will Hinkelman <will@Wills-MacBook-Pro-2.local>
2019-10-25 10:55:34 -0700

    initial board layout created

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
 /*----- constants -----*/
 
-
+const PLAYER_X = 1;
+const PLAYER_O = -1;
+const EMPTY = 0;
 
 /*----- app's state (variables) -----*/
-
+let turn, winner, board;
 
 
 /*----- cached element references -----*/
 
-
+const replayBtn = document.querySelector('button');
 
 /*----- event listeners -----*/
 
 
 
 /*----- functions -----*/
+
+init();
+
+//winner if add up to |3|
+// board = [
+//     [0, 0, 0],
+//     [0, 0, 0],
+//     [0, 0, 0]
+// ]
+
+function inti() {
+    board = [
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]
+    ];
+    turn = PLAYER_X;
+    winner = '';
+}
